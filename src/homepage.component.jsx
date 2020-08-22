0aaae01732455b33191ad450cf821fd3b5c8ca97
Faysal <Faysal@DESKTOP-4F7L43J>
2020-08-22 19:22:18 +0500

    Home Page Setup-Up+ Add SASS

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+const HomePage = () => (
+    <div className='Homepage'>
+        <div className='Main-Menu'>
+            <div className='Menu-Item'>
+                <div className='Content'>
+                    <h1 className='Title'>HATS</h1>
+                    <span className='Subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='Content'>
+                    <h1 className='Title'>MENS</h1>
+                    <span className='Subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='Content'>
+                    <h1 className='Title'>JACKETS</h1>
+                    <span className='Subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='Content'>
+                    <h1 className='Title'>SNEAKERS</h1>
+                    <span className='Subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='Menu-Item'>
+                <div className='Content'>
+                    <h1 className='Title'>WOMEN</h1>
+                    <span className='Subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+)
+
+export default HomePage;
