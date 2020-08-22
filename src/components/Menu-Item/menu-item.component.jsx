c8540e678ae5c4b5e00ed0751d06cb92012f020a
Faysal <Faysal@DESKTOP-4F7L43J>
2020-08-22 20:50:47 +0500

    Components&Pages Added

---
+++ src/components/Menu-Item/menu-item.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import '../Menu-Item/menu-item.styles.scss'
+
+//destructing title {title}
+const MenuItem = ({ title, imageUrl,size }) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} className={`${size} Menu-Item`}>
+        <div className='Content'>
+            <h1 className='Title'>{title}</h1>
+            <span className='Subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+
+
+);
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MainMenu from '../../components/Main-Menu/main-menu.component'
+import './homepage.styles.scss'
+const HomePage = () => (
+    <div className='Homepage'>
+       <MainMenu />
+
+    </div>
+
+)
+
+export default HomePage;
