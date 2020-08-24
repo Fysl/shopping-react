3780d7e5daa132784e6482c021b79440b083ce70
Faysal <Faysal@DESKTOP-4F7L43J>
2020-08-25 04:54:59 +0500

    collectionAdded

--- src/App.js
+++ src/App.js
@@ -4,21 +4,15 @@ import {Switch,Route} from 'react-router-dom'
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-
-const hatpage =()=>(
-
-<div>
-  <h1>HATS</h1>
-</div>
-);
-
+import ShopPage from './pages/shop/shop.component'
+ 
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={HomePage} />
-     <Route path='/hats' component={hatpage} />
+     <Route path='/shop' component={ShopPage} />
       </Switch>
     
     </div>
