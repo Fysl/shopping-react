98f719d258fb64a7ad95e7c86beb54d924a4731d
Faysal <Faysal@DESKTOP-4F7L43J>
2020-08-26 02:37:39 +0500

    collectionItem Added + SASS

--- src/components/preview-collection/collectionpreview.component.js
+++ src/components/preview-collection/collectionpreview.component.js
@@ -1,20 +1,19 @@
 import React from 'react'
 //import {withRouter} from 'react-router-dom'
-
-//import '../preview-collection/collectionpreview.scss'
+import CollectionItem from '../collection-item/collectionitem.component'
+import '../preview-collection/collectionpreview.styles.scss'
 
 const CollectionPreview =({title,items}) => (
 
-    <div className='collectionpreview'>
+    <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
 {
     items
     .filter((item,idx)=>idx < 4)
-    .map(item => (
-    <div key={item.id}>{item.name}</div>
-    ))
-}
+    .map(({id,...otherItemProps}) => (
+    <CollectionItem key={id}{...otherItemProps} /> 
+    ))}
 </div>
     </div>
 )
